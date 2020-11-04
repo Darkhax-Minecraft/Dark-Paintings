9beea0f00c3f67a3f46c3bd7bd330a4227984946
Tyler Hancock <darklime@live.ca>
2020-11-04 16:40:27 -0700

    Ported to 1.16.4

--- src/main/java/net/darkhax/darkpaintings/DarkPaintings.java
+++ src/main/java/net/darkhax/darkpaintings/DarkPaintings.java
@@ -31,6 +31,7 @@ public class DarkPaintings {
         registry.register(this.createPainting("bubbles", 16, 16));
         registry.register(this.createPainting("afternoon_volcano", 16, 32));
         registry.register(this.createPainting("the_wheel", 32, 32));
+        registry.register(this.createPainting("mr_moon", 16, 16));
     }
     
     private PaintingType createPainting (String id, int width, int height) {
