c51043ea4b859069b2a7cb68f07a8ff75fc31daf
Tyler Hancock <darklime@live.ca>
2023-11-16 19:33:37 -0700

    Port to 1.20.2 and add NeoForge support.

---
+++ neoforge/src/main/java/net/darkhax/darkpaintings/DarkPaintingsNeoForge.java
@@ -0,0 +1,12 @@
+package net.darkhax.darkpaintings;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class DarkPaintingsNeoForge {
+
+    public DarkPaintingsNeoForge() {
+
+        DarkPaintingsContent.init();
+    }
+}
