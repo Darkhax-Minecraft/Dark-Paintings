5c6ac6368600387e26a173c210e8ddd9a03487d5
Tyler Hancock <darklime@live.ca>
2025-10-10 16:06:34 -0600

    Add new paintings for fall and backport paintings from newer versions of the mod.

--- common/src/main/java/net/darkhax/darkpaintings/DarkPaintingsContent.java
+++ common/src/main/java/net/darkhax/darkpaintings/DarkPaintingsContent.java
@@ -22,6 +22,8 @@ public final class DarkPaintingsContent extends RegistryDataProvider {
         this.painting("scarlet_moon", 16, 16);
         this.painting("tetromino", 16, 32);
         this.painting("daybreak", 32, 16);
+        this.painting("omens_wing", 32, 32);
+        this.painting("silent_watcher", 16, 32);
 
         // mmg28rtt
         this.painting("planets", 32, 16);
@@ -55,12 +57,26 @@ public final class DarkPaintingsContent extends RegistryDataProvider {
         this.painting("kerstball", 16, 16);
         this.painting("snowman_scenery", 32, 16);
         this.painting("santa_moon", 32, 32);
+        this.painting("acorn", 16, 16);
+        this.painting("chimnee", 16, 32);
+        this.painting("fall_forest", 32, 16);
+        this.painting("foreck_skull", 32, 16);
+        this.painting("hoot", 16, 16);
+        this.painting("pumkin", 16, 16);
+        this.painting("shroom", 16, 16);
+        this.painting("sunset", 16, 32);
+        this.painting("waterway", 32, 32);
 
         // punpudle
         this.painting("the_secret_neighborhood", 32, 32);
         this.painting("last_dreams_at_candyland", 32, 16);
         this.painting("the_most_sweet_candy", 16, 16);
         this.painting("sir_ramfrez_yarn_paradise", 16, 16);
+
+        // Superkat32
+        this.painting("salmon_run", 16, 16);
+        this.painting("snail_aboard", 32, 16);
+        this.painting("nighttime_calico", 16, 32);
     }
 
     private void painting(String id, int width, int height) {
