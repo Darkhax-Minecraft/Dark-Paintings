404a127110512ac896a73bfd112fd7d59c58d170
Tyler Hancock <darklime@live.ca>
2026-02-01 02:19:07 -0700

    Port to Minecraft 1.21.11

--- common/src/main/java/net/darkhax/darkpaintings/common/impl/DarkPaintingsMod.java
+++ common/src/main/java/net/darkhax/darkpaintings/common/impl/DarkPaintingsMod.java
@@ -1,6 +1,5 @@
 package net.darkhax.darkpaintings.common.impl;
 
-import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +8,4 @@ public class DarkPaintingsMod {
     public static final String MOD_ID = "darkpaintings";
     public static final String MOD_NAME = "DarkPaintings";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
-
-    public static ResourceLocation id(String path) {
-        return ResourceLocation.tryBuild(MOD_ID, path);
-    }
 }
